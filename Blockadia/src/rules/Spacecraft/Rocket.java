a5e713e74c0c461913651af851edcc1b3cf6309d
alex.yang <alex.yang@WRK-IT-07.zepower.com>
2013-11-08 13:35:11 -0800

    Working on Spacecraft's shooting method. Implemented the cooldown function(brief version). Ready for merge

--- Blockadia/src/rules/Spacecraft/Rocket.java
+++ Blockadia/src/rules/Spacecraft/Rocket.java
@@ -1,40 +1,114 @@
 package rules.Spacecraft;
 
+import org.jbox2d.dynamics.Body;
+
 public class Rocket {
+  public static final int RocketGroupIndex = -5;
   public static enum RocketType{
 	NormalBullet, Laser, DoubleLaser, Rocket, Flame, SpreadBullet, ChaseBullet;
   }
   
+  public static final String OriginalID = "Rocket-(0000)";
+
+  //cooldown or recharging
+  public static final int NormalBulletCD = 1000;
+  public static final int LaserCD = 2000;
+  public static final int DoubleLaserCD = 2000;
+  public static final int RocketCD = 1000;
+  public static final int FlameCD = 2000;
+  public static final int SpreadBulletCD = 1000;
+  public static final int ChaseBulletCD = 1000;
   public static final RocketType DefaultRocketType = RocketType.NormalBullet;
+
+  //num of bullets per shot
+  public static final int NormalBulletNum = 2;
+  public static final int LaserNum = 2;
+  public static final int DoubleLaserNum = 4;
+  public static final int RocketNum = 2;
+  public static final int FlameNum = 2;
+  public static final int SpreadBulletBum = 3;
+  public static final int ChaseBulletNum = 2;
   
+  //TODO: base damage
+  //TODO: how long it last(for laser, double laser, flame)
+  
+  private Body rocketBody;
   private RocketType type;
-  /**In nanosecond*/
-  private int cooldown;
-  private float speed;
   private float baseDamage;
-  
+  private String id;
+
   public Rocket(){
-	type = DefaultRocketType;
-	cooldown = 1000000000;
+	this.type = DefaultRocketType;
+	this.baseDamage = 50f;
+	this.id = OriginalID;
   }
-  
+
   /**
    * This is called after the any piece of the rocket hits an object in the game
    * */
   public void applyDamage(Object object){
 	if(object instanceof Spacecraft){
-	  
+	  applyDamage((Spacecraft)object);
 	}
 	else if(object instanceof Obstacle){
 	  if(object instanceof Bound){
-		
+		applyDamage((Bound)object);
 	  }
 	  else{
-		
+		applyDamage((Obstacle)object);
 	  }
 	}
 	else if(object instanceof Monster){
-	  
+	  applyDamage((Monster)object);
 	}
+  }
+
+  private void applyDamage(Spacecraft spacecraft){
+
+  }
+
+  private void applyDamage(Bound bound){
+
+  }
+
+  private void applyDamage(Obstacle obstacle){
+
+  }
+
+  private void applyDamage(Monster monster){
+
+  }
+
+  public Body getRocketBody() {
+	return rocketBody;
+  }
+
+  public void setRocketBody(Body rocketBody) {
+	this.rocketBody = rocketBody;
+	this.rocketBody.setUserData(this);
+  }
+
+  public float getBaseDamage() {
+	return baseDamage;
+  }
+
+  public void setBaseDamage(float baseDamage) {
+	this.baseDamage = baseDamage;
+  }
+
+  public RocketType getType() {
+	return type;
+  }
+
+  public void setType(RocketType type) {
+	this.type = type;
+  }
+
+  public String getId() {
+	return id;
+  }
+
+  public void setId(String id) {
+	this.id = id;
   }
 }
--- Blockadia/src/rules/Spacecraft/Spacecraft.java
+++ Blockadia/src/rules/Spacecraft/Spacecraft.java
@@ -3,30 +3,42 @@ package rules.Spacecraft;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.jbox2d.collision.shapes.CircleShape;
+import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.FixtureDef;
+import org.jbox2d.dynamics.World;
+
+import rules.Spacecraft.Rocket.RocketType;
+import utility.Log;
 
 public class Spacecraft {
-  
+
   public static final int SpacecraftGroupIndex = -1;
-  
+
   private Body spacecraftBody;
   private String id;
   private int level;
   private int hp;
-  private int maxRockets;
 
   //Rocket
+  private int maxRockets;
+  private RocketType rocketType;
   private Map<String, Rocket> rockets;
-  
+  private boolean onCD;
+
   public Spacecraft(){
 	id = "Alex the sky-raider";
 	spacecraftBody = null;
 	level = 0;
 	hp = 100;
+	rocketType = RocketType.NormalBullet;
 	rockets = new HashMap<String, Rocket>();
 	maxRockets = getNumOfRocketsByLevel();
   }
-  
+
   private int getNumOfRocketsByLevel() {
 	return level + 5;
   }
@@ -34,46 +46,121 @@ public class Spacecraft {
   public Body getSpacecraftBody() {
 	return spacecraftBody;
   }
-  
+
   public void setSpacecraftBody(Body spacecraftBody) {
 	this.spacecraftBody = spacecraftBody;
 	this.spacecraftBody.setUserData(this);
   }
-  
+
   public int getLevel() {
 	return level;
   }
-  
+
   public void setLevel(int level) {
 	this.level = level;
   }
-  
+
   public int getHp() {
 	return hp;
   }
-  
+
   public void setHp(int hp) {
 	this.hp = hp;
   }
-  
+
   public String getId() {
 	return id;
   }
-  
+
   public void setId(String id) {
 	this.id = id;
   }
-  
+
   public Map<String, Rocket> getRockets() {
 	return rockets;
   }
 
-  public void setRockets(Map<String, Rocket> rockets) {
-	this.rockets = rockets;
+  public RocketType getRocketType() {
+	return rocketType;
+  }
+
+  public void setRocketType(RocketType rocketType) {
+	this.rocketType = rocketType;
+  }
+
+  /**CD is on millisecond*/
+  public int getCooldown() {
+	int cd = 0;
+	switch(rocketType){
+	case NormalBullet:
+	  cd = Rocket.NormalBulletCD;
+	  break;
+	case Laser:
+	  cd = Rocket.LaserCD;
+	  break;
+	case DoubleLaser:
+	  cd = Rocket.DoubleLaserCD;
+	  break;
+	case Rocket:
+	  cd = Rocket.RocketCD;
+	  break;
+	case Flame:
+	  cd = Rocket.FlameCD;
+	  break;
+	case SpreadBullet:
+	  cd = Rocket.SpreadBulletCD;
+	  break;
+	case ChaseBullet:
+	  cd = Rocket.ChaseBulletCD;
+	  break;
+	default:
+	  cd = Rocket.NormalBulletCD;
+	  break;
+	}
+	return cd;
+  }
+
+  public boolean isOnCD() {
+	return onCD;
+  }
+
+  public void setOnCD(boolean onCD) {
+	this.onCD = onCD;
   }
 
   public int getMaxRockets() {
 	return maxRockets;
+  }
+
+  private int getRocketNumPerShot(){
+	int rocketNum = 0;
+	switch(rocketType){
+	case NormalBullet:
+	  rocketNum = Rocket.NormalBulletNum;
+	  break;
+	case Laser:
+	  rocketNum = Rocket.LaserNum;
+	  break;
+	case DoubleLaser:
+	  rocketNum = Rocket.DoubleLaserNum;
+	  break;
+	case Rocket:
+	  rocketNum = Rocket.RocketNum;
+	  break;
+	case Flame:
+	  rocketNum = Rocket.FlameNum;
+	  break;
+	case SpreadBullet:
+	  rocketNum = Rocket.SpreadBulletBum;
+	  break;
+	case ChaseBullet:
+	  rocketNum = Rocket.ChaseBulletNum;
+	  break;
+	default:
+	  rocketNum = Rocket.NormalBulletNum;
+	  break;
+	}
+	return rocketNum;
   }
 
   public void levelUp(){
@@ -89,5 +176,97 @@ public class Spacecraft {
 	if(level != anotherCraft.getLevel()) return false;
 
 	return true;
+  }
+
+  public boolean shoot(World world){
+	//check cool down and max num rockets
+	if(this.onCD){
+	  Log.print("Rocket on CD, not ready to shoot");
+	  return false;
+	}
+
+	if(this.getRockets().size() >= (this.getMaxRockets() * this.getRocketNumPerShot())){
+	  Log.print("Rockets reloading, not ready to shoot");
+	  return false;
+	}
+
+	//Spawn points
+	Vec2 leftSpawnPt = null;
+	Vec2 leftSpawnPt2 = null;
+	Vec2 rightSpawnPt = null;
+	Vec2 rightSpawnPt2 = null;
+	Vec2 centerSpawnPt = null;
+	int rocketNum = this.getRocketNumPerShot();
+	if(rocketNum == 2){
+	  leftSpawnPt = this.getSpacecraftBody().getWorldPoint(new Vec2(-.55f,-.4f));
+	  rightSpawnPt = this.getSpacecraftBody().getWorldPoint(new Vec2(.55f,-.4f));
+	}
+	else if (rocketNum == 3){
+	  //TODO
+	}
+	else if (rocketNum == 4){
+	  //TODO
+	}
+	else{
+	  Log.print("Unexpected Error: Invalid number of rockets per shot.");
+	  return false;
+	}
+
+	Rocket rocket = new Rocket();
+	rocket.setType(this.rocketType);
+	rocket.setBaseDamage(getBaseDamageByRocket());
+
+	String rocketId = rocket.getId();
+	if(rocketNum == 2 && leftSpawnPt != null && rightSpawnPt != null){
+	  Vec2 velocity = spacecraftBody.getLinearVelocity().clone();
+	  if(spacecraftBody.getLinearVelocity().length() <= 30f){
+		//velocity = 45f
+		velocity.normalize();
+		velocity.mulLocal(45f);
+	  }
+	  else{
+		//velocity = currVelocity + 15;
+		Vec2 increment = velocity.clone();
+		increment.normalize();
+		increment.mulLocal(15f);
+		velocity.addLocal(increment);
+	  }
+
+	  if(rocket.getType() == RocketType.NormalBullet){
+		//left bullet
+		BodyDef bd = new BodyDef();
+		bd.type = BodyType.DYNAMIC;
+		bd.linearVelocity.set(velocity);
+		bd.bullet = true;
+		bd.position.set(leftSpawnPt);
+		CircleShape bullet = new CircleShape();
+		bullet.setRadius(.1f);
+		FixtureDef fd = new FixtureDef();
+		fd.shape = bullet;
+		fd.density = 1f;
+		fd.filter.groupIndex = Rocket.RocketGroupIndex;
+		//Unique id
+		while(rockets.containsKey(rocketId)){
+		  rocketId = Rocket.OriginalID;
+		  int rand = (int)(Math.random()*10000);
+		  rocketId = rocketId.replace("0000", ""+rand);
+		}
+		rocket.setId(rocketId);
+		rocket.setRocketBody(world.createBody(bd));
+		rocket.getRocketBody().createFixture(fd);
+		rockets.put(rocket.getId(), rocket);
+		
+		//right bullet
+		bd = new BodyDef();
+		bd.position.set(rightSpawnPt);
+		//TODO
+	  }
+	}
+	return true;
+  }
+
+  private float getBaseDamageByRocket() {
+	// TODO 
+	return 50f;
   }
 }
